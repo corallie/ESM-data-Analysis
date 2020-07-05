e1eda37d4fecfc2314c5875648b7e388bfed7dbb
Pauline Mialhe <pauline.mialhe@univ-reunion.fr>
2020-07-05 19:42:30 +0200

    Phases nb added

--- function/table_phases.R
+++ function/table_phases.R
@@ -17,6 +17,7 @@ table_phases <- function(phases, save_at = NULL) {
   phases_asc_dur_tot  <- tapply(phases_asc$diff_t, phases_asc$id, sum)
   phases_asc_dur_mean <- tapply(phases_asc$diff_t, phases_asc$id, mean)
   phases_asc_slope_e  <- tapply(phases_asc$slope_e, phases_asc$id, mean)
+  phases_asc_nb       <- length(phases_asc)
   # 
   # 
   # Caractéristiques des phases descendantes :
@@ -26,6 +27,7 @@ table_phases <- function(phases, save_at = NULL) {
   phases_desc_dur_tot  <- tapply(phases_desc$diff_t, phases_desc$id, sum)
   phases_desc_dur_mean <- tapply(phases_desc$diff_t, phases_asc$id, mean)
   phases_desc_slope_e  <- tapply(phases_desc$slope_e, phases_asc$id, mean)
+  phases_desc_nb       <- length(phases_desc)
   # 
   # 
   # Caractéristiques des phases plates :
@@ -34,12 +36,13 @@ table_phases <- function(phases, save_at = NULL) {
   phases_plat_auc_mean <- tapply(phases_plat$auc_to_min, phases_plat$id, mean)
   phases_plat_dur_tot  <- tapply(phases_plat$diff_t, phases_plat$id, sum)
   phases_plat_dur_mean <- tapply(phases_plat$diff_t, phases_plat$id, mean)
+  phases_plat_nb       <- length(phases_plat)
   # 
   # 
   # On regroupe le tout : 
-  phases_features <- rbind(phases_asc_auc_tot, phases_asc_auc_mean, phases_asc_dur_tot, phases_asc_dur_mean, phases_asc_slope_e, 
-                           phases_desc_auc_tot, phases_desc_auc_mean, phases_desc_dur_tot, phases_desc_dur_mean, phases_desc_slope_e, 
-                           phases_plat_auc_tot, phases_plat_auc_mean, phases_plat_dur_tot, phases_plat_dur_mean)
+  phases_features <- rbind(phases_asc_auc_tot, phases_asc_auc_mean, phases_asc_dur_tot, phases_asc_dur_mean, phases_asc_slope_e, phases_asc_nb, 
+                           phases_desc_auc_tot, phases_desc_auc_mean, phases_desc_dur_tot, phases_desc_dur_mean, phases_desc_slope_e, phases_desc_nb, 
+                           phases_plat_auc_tot, phases_plat_auc_mean, phases_plat_dur_tot, phases_plat_dur_mean, phases_plat_nb)
   # 
   # 
   # On rajoute id dans le nom de colonne pour que ça soit bien explicite : 
